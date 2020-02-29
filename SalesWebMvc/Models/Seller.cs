73540ec63f7da75f552a4a202f54bd7ba32a4159
Daniel Júlio <daniel98julio@gmail.com>
2020-02-29 14:03:24 -0300

    Validação

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -6,18 +6,30 @@ using System.Linq;
 namespace SalesWebMvc.Models {
     public class Seller {
         public int Id { get; set; }
+
         [Display(Name = "Nome")]
+        [Required(ErrorMessage = "{0} é obrigatório.")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} deve ter no Mínimo {2} caracteres.")]
         public string Name { get; set; }
+
         [Display(Name = "E-mail")]
+        [Required(ErrorMessage = "{0} é obrigatório.")]
+        [EmailAddress(ErrorMessage = "Insira um E-mail Válido")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
         [Display(Name = "Data de Aniversário")]
+        [Required(ErrorMessage = "{0} é obrigatório.")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
+
         [Display(Name = "Salário Base")]
+        [Required(ErrorMessage = "{0} é obrigatório.")]
+        [Range(1039.0, 50000.0, ErrorMessage = "O {0} Mínimo é de R${1}.")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
+
         [Display(Name = "Departamento")]
         public Department Department { get; set; }
         public int DepartmentId {get; set; }
