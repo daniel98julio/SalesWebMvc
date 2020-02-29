eaf160b4b2b3fecfcf8b9b2e8bfac32f6f251b68
Daniel Júlio <daniel98julio@gmail.com>
2020-02-29 13:15:51 -0300

    Retornando Customs Errors

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -1,18 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SalesWebMvc.Models;
+using SalesWebMvc.Models.ViewModels;
 using SalesWebMvc.Services;
+using SalesWebMvc.Services.Exceptions;
 
-namespace SalesWebMvc.Controllers
-{
+namespace SalesWebMvc.Controllers {
     public class SellersController:Controller {
         private readonly SellerService _sellerService;
+        private readonly DepartmentService _departmentService;
 
-        public SellersController(SellerService sellerService) {
+        public SellersController(SellerService sellerService, DepartmentService departmentService) {
             _sellerService = sellerService;
+            _departmentService = departmentService;
         }
 
         public IActionResult Index() {
@@ -22,15 +26,90 @@ namespace SalesWebMvc.Controllers
         }
 
         public IActionResult Create() {
-            return View();
+            var departments = _departmentService.FindAll();
+            var viewModel = new SellerFormViewModel { Departments = departments };
+            return View(viewModel);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-
         public IActionResult Create(Seller seller) {
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int? id) {
+            if(id == null) {
+                return RedirectToAction(nameof(Error), new { message = "O ID é Nulo."});
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if(obj == null) {
+                return RedirectToAction(nameof(Error), new { message = "ID Não Existe." });
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id) {
+            _sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Details(int? id) {
+            if(id == null) {
+                return RedirectToAction(nameof(Error), new { message = "O ID é Nulo" });
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if(obj == null) {
+                return RedirectToAction(nameof(Error), new { message = "ID Não Existe." });
+            }
+
+            return View(obj);
+        }
+
+        public IActionResult Edit(int? id) {
+            if(id == null) {
+                return RedirectToAction(nameof(Error), new { message = "O ID é Nulo." });
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if(obj == null) {
+                return RedirectToAction(nameof(Error), new { message = "ID Não Existe" });
+            }
+
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+            return View(viewModel);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller) {
+            if(id != seller.Id) {
+                return RedirectToAction(nameof(Error), new { message = "Os IDs Não Correspondem." });
+            }
+
+            try {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+
+            catch(ApplicationException e) {
+                return RedirectToAction(nameof(Error), new { message = e.Message });
+            }
+        }
+
+        public IActionResult Error(string message) {
+            var viewModel = new ErrorViewModel {
+                Message = message,
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            };
+            return View(viewModel);
         }
     }
 }
